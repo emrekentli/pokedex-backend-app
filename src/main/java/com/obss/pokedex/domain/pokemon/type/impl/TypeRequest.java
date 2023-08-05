5090eace639a81339ad103ddd7c81859ac9fc736
Emre <emrekentlii@gmail.com>
2023-08-06 01:39:20 +0300

    type req fix

--- src/main/java/com/obss/pokedex/domain/pokemon/type/impl/TypeRequest.java
+++ src/main/java/com/obss/pokedex/domain/pokemon/type/impl/TypeRequest.java
@@ -1,5 +1,6 @@
 package com.obss.pokedex.domain.pokemon.type.impl;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.obss.pokedex.domain.pokemon.type.api.TypeDto;
 import lombok.Builder;
 import lombok.Data;
@@ -8,6 +9,10 @@ import lombok.Data;
 @Builder
 public class TypeRequest {
     private final String name;
+
+    public TypeRequest(@JsonProperty("name") String name) {
+        this.name = name;
+    }
 
     public TypeDto toDto() {
         return TypeDto.builder()
